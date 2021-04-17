841b1d8dc063153d8f78ae7488c797b8e1206bcf
Reedman20 <swan.gunnarj@gmail.com>
2021-04-17 14:28:45 -0500

    minor fix ups

--- main/models.py
+++ main/models.py
@@ -3,6 +3,6 @@ from mapbox_location_field.models import LocationField
 
 
 class LocationPickerModel(models.Model):
-    location = LocationField(map_attrs={"style": "mapbox://styles/mightysharky/cjwgnjzr004bu1dnpw8kzxa72", "center": [-91.5550, 41.6627], "marker_color": 'blue'})
+    location = LocationField(map_attrs={"style": "mapbox://styles/mightysharky/cjwgnjzr004bu1dnpw8kzxa72", "center": [-91.5550, 41.6627], "marker_color": 'red'})
     radius = models.IntegerField()
-    hours = models.IntegerField()
+    days = models.IntegerField()
