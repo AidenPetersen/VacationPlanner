44d4d905cebc02d93f0a6754bd4660828480e003
SamNicklez <SamuelNicklaus1@gmail.com>
2021-04-17 13:43:31 -0500

    Map pog

--- main/models.py
+++ main/models.py
@@ -3,6 +3,6 @@ from mapbox_location_field.models import LocationField
 
 
 class LocationPickerModel(models.Model):
-    location = LocationField(map_attrs={"style": "mapbox://styles/mightysharky/cjwgnjzr004bu1dnpw8kzxa72", "center": (17.031645, 51.106715)})
+    location = LocationField(map_attrs={"style": "mapbox://styles/mightysharky/cjwgnjzr004bu1dnpw8kzxa72", "center": [-91.5550, 41.6627], "marker_color": 'blue'})
     radius = models.IntegerField()
     time = models.TimeField()
