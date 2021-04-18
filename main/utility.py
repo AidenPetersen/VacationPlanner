a51cea799b90b9311f25bc22d7336dfcb36e833b
AidenPetersen <AidenPetersen@protonmail.com>
2021-04-17 23:45:28 -0500

    godcurrency

--- main/utility.py
+++ main/utility.py
@@ -1,29 +1,46 @@
+from asyncio import run, wait
+
 from yelpapi import YelpAPI
 import requests
 from math import radians, sin, cos, acos
+import threading
 
 
-def get_yelp_review(latitude: float, longitude: float, name: str) -> float:
-    key = "JwyKsopjKmwG41IpB_mUzDtY7JSiNZsPGpiWzdlGrDZ5GHfXZE0kCx11sPtH-epP20pBnbUXxj4Nhz_AY0cKzTBktSbu2CjnfLfyyM" \
-          "-wsUeleiAM1YStOBFdvHl7YHYx "
-    yelp_api = YelpAPI(key)
+def get_yelp_review(latitude: float, longitude: float, name: str, token_num: int) -> float:
+    tokens = [
+        "JwyKsopjKmwG41IpB_mUzDtY7JSiNZsPGpiWzdlGrDZ5GHfXZE0kCx11sPtH-epP20pBnbUXxj4Nhz_AY0cKzTBktSbu2CjnfLfyyM-wsUeleiAM1YStOBFdvHl7YHYx ",
+        "pUUE2sp9znWz3doMfBQuO9NpsjrXxd1buJ5ECvqcuUmgnDYxf5ov375i9xwThXEZ_7sQJuPN_djpXGF9GMChQWgrpsxw1lyytnBh4V3W3IK_CYIK4m_2bALI8bV7YHYx",
+        "rzbaE_ls0PITatDUuJrGK_sxkXRsoCfkflk8KAr_USpYfdy-8hV_At3W8qQdAtC3Jw6F6r8T38zt_QKX8LLxBwefZxX3P1QnhKYN1mUr-e5or5fpNSoSgufLG7d7YHYx"]
+    yelp_api = YelpAPI(tokens[token_num])
     response = yelp_api.search_query(latitude=latitude, longitude=longitude, term=name, radius=100, limit=1)
     print(name)
     try:
         return response['businesses'][0]['rating']
-    except (IndexError,TypeError):
+    except (IndexError, TypeError):
         return 0
 
 
 def format_attractions(latitude: float, longitude: float, radius: int) -> list:
     attractions_list = otm_get("radius", radius_query(latitude, longitude, radius))
-    attractions_ratings = [get_yelp_review(x['point']['lat'], x['point']['lon'], x['name']) for x in attractions_list]
+
+    # attractions_ratings = [get_yelp_review(x['point']['lat'], x['point']['lon'], x['name']) for x in attractions_list]
+
+    async def add_review(arr, lat, lon, n, t):
+        arr.append(get_yelp_review(lat, lon, n, t))
+
+    attractions_ratings = []
+    counter = 0
+    for x in attractions_list:
+        run(add_review(attractions_ratings, x['point']['lat'], x['point']['lon'], x['name'], counter))
+        counter += 1
+        counter %= 3
+
     attractions = []
     outer_index = 0
     inner_index = 0
     for rating in attractions_ratings:
         print(rating)
-        if (rating > 3.5):
+        if rating > 3.5:
             name = attractions_list[outer_index]['name']
             if name:
                 attractions.append((attractions_list[outer_index]['name'], rating, attractions_list[outer_index]))
